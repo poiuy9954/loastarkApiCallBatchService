65ba36f2745e90fa677c5059595119054c29ed4d
gimjinho <poiuy9954@gamil.com>
2024-12-26 09:59:34 +0900

    first commited

---
+++ src/main/java/com/laapicallbat/lostarkapicallbatchservice/LostarkApiCallBatchServiceApplication.java
@@ -0,0 +1,14 @@
+package com.laapicallbat.lostarkapicallbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LostarkApiCallBatchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostarkApiCallBatchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laapicallbat/lostarkapicallbatchservice/LostarkApiCallBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laapicallbat.lostarkapicallbatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkApiCallBatchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
