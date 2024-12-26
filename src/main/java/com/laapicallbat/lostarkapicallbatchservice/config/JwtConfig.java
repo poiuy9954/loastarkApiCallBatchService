69b73801fa77a069823531a6c82007631ab60a8e
gimjinho <poiuy9954@gamil.com>
2024-12-26 10:00:35 +0900

    JwtConfig 추가 - jwt관련 정보 저장

---
+++ src/main/java/com/laapicallbat/lostarkapicallbatchservice/config/JwtConfig.java
@@ -0,0 +1,19 @@
+package com.laapicallbat.lostarkapicallbatchservice.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtConfig {
+
+    private String token;
+    private String prefix;
+}
+
+
