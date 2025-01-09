7c66ef69849c3606714f4784061893eeada91533
gimjinho <poiuy9954@gamil.com>
2025-01-09 12:10:14 +0900

    config 파일들 및 암호화 필요데이터 configServer로 이전

--- src/main/java/com/laapicallbat/lostarkapicallbatchservice/aa/config/JwtConfig.java
+++ src/main/java/com/laapicallbat/lostarkapicallbatchservice/aa/config/JwtConfig.java
@@ -3,16 +3,19 @@ package com.laapicallbat.lostarkapicallbatchservice.aa.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "jwt")
+//@ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
 
+    @Value("${jwt.token}")
     private String token;
+    @Value("${jwt.prefix}")
     private String prefix;
 }
 
